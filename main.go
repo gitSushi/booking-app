83d7011301eae2956737f34714b7a598564a7c15
gitSushi <lossushi37@gmail.com>
2023-01-15 22:23:40 +0000

    up to types

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "our conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "our conference" // syntactic sugar for implicit variables
+	const conferenceTickets uint8 = 50 // There cannot be a negative total ticket
+	var remainingTickets uint16 = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking app")
-	fmt.Println("We have", remainingTickets, "tickets still available out of the", conferenceTickets, ".")
+	fmt.Printf("type of conferenceName is %T\n", conferenceName)       // string
+	fmt.Printf("type of conferenceTickets is %T\n", conferenceTickets) // int
+	fmt.Printf("type of remainingTickets is %T\n", remainingTickets)   // int
+
+	fmt.Printf("Welcome to %v booking app\n", conferenceName)
+	fmt.Printf("We have %v tickets still available out of the %v.\n", remainingTickets, conferenceTickets)
 	fmt.Println("Get your ticket here to attend")
 
+	var username string
+	var userTickets int
+	// ask user for their name
+
+	username = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User named %v, booked %v tickets.\n", username, userTickets)
 }
