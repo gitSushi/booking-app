d9f12f3fa1938688c8383fa0b3b8cd5524182b71
gitSushi <lossushi37@gmail.com>
2023-01-15 13:17:01 +0000

    tickets added

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Welcome to your booking app")
+	var conferenceName = "our conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking app")
+	fmt.Println("We have", remainingTickets, "tickets still available out of the", conferenceTickets, ".")
+	fmt.Println("Get your ticket here to attend")
+
 }
